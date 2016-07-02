076de8b1260c4ba29744d1fa10b7e3dacc610cc6
Cody Brocious <cody.brocious@gmail.com>
2016-07-02 14:55:01 -0600

    Bumped version and pushed to PyPi.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='PyGenic',
-		version='0.1', 
+		version='0.2', 
 		description='Code generation library', 
 		author='Cody Brocious', 
 		author_email='cody.brocious@gmail.com', 
